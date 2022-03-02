99eba00514d1754ccee07c4193867ed2d80918ef
Princi Saspara <51781106+princisaspara@users.noreply.github.com>
2022-03-01 18:16:55 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,31 +1,86 @@
-import streamlit as st
+
 import pandas as pd
+import yfinance as yf
+
 import altair as alt
+from pandas_datareader import data
+import streamlit as st
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set_style('whitegrid')
+plt.style.use("fivethirtyeight")
+# For reading stock data from yahoo
+from datetime import datetime
 
-st.title("Let's analyze some Penguin Data 🐧📊.")
 
-@st.cache  # add caching so we load the data only once
-def load_data():
-    # Load the penguin data from https://github.com/allisonhorst/palmerpenguins.
-    penguins_url = "https://raw.githubusercontent.com/allisonhorst/palmerpenguins/v0.1.0/inst/extdata/penguins.csv"
-    return pd.read_csv(penguins_url)
 
-df = load_data()
+st.title("Did the crypto volatility affect stock prices?")
+st.caption("1. Here, we can select Bitcoin and any other stock for comparison. Select two, or multiple stocks for comparison")
 
-st.write("Let's look at raw data in the Pandas Data Frame.")
 
-st.write(df)
 
-st.write("Hmm 🤔, is there some correlation between body mass and flipper length? Let's make a scatterplot with [Altair](https://altair-viz.github.io/) to find.")
+stock_list = ('BTC-USD','ETH-USD','TSLA','AAPL', 'GOOG', 'MSFT', 'AMZN')
 
-chart = alt.Chart(df).mark_point().encode(
-    x=alt.X("body_mass_g", scale=alt.Scale(zero=False)),
-    y=alt.Y("flipper_length_mm", scale=alt.Scale(zero=False)),
-    color=alt.Y("species")
+dropdown = st.multiselect('Select stocks for comparison', stock_list)
+
+start = st.date_input('Start',value=pd.to_datetime('2021-01-01'))
+end = st.date_input('End',value=pd.to_datetime('today'))
+
+def stock_returns(df):
+    rel=df.pct_change()
+    cumret = (1+rel).cumprod() - 1
+    cumret = cumret.fillna(0)
+    return cumret
+
+if len(dropdown) > 0:
+    df = stock_returns(yf.download(dropdown,start,end)['Adj Close'])
+    st.line_chart(df)
+
+
+
+
+
+df = stock_returns(yf.download(stock_list,start,end)['Adj Close'])
+
+
+#Interaction = stock_returns(yf.download(stock_list,datetime(end.year - 1, end.month, end.day),end)['Adj Close'])
+
+df.reset_index(inplace=True)
+df = df.rename(columns = {'index':'Date'})
+
+df= df.melt(id_vars="Date")
+df=df.rename(columns={"Date": "date", "variable": "company", "value": "return"})
+
+portfolio=df
+
+interval = alt.selection_interval(encodings=['x'])
+
+scatter = alt.Chart(portfolio).mark_line().encode(
+    x='date',
+    y='return',
+    color='company',
 ).properties(
-    width=600, height=400
-).interactive()
+    width=550,
+    selection=interval
+)
 
-st.write(chart)
 
-st.markdown("This project was created by Student1 and Student2 for the [Interactive Data Science](https://dig.cmu.edu/ids2022) course at [Carnegie Mellon University](https://www.cmu.edu).")
+bar = alt.Chart(portfolio).mark_bar().encode(
+    x='average(return)',
+    y='company',
+    color='company',
+  ).properties(
+      width=550,  
+).transform_filter(
+    interval
+)
+
+return_comparison_viz = scatter & bar
+
+
+st.title("Did the crypto volatility affect stock returns?")
+st.caption("Both the visualization represent stock returns over the last year. Click on the graph and select a time-period you want to explore. Slide your selection across the graph, to view the change in returns for each stock at the bottom ")
+
+st.write(return_comparison_viz)
+
+st.markdown("This project was created by Urvish Thakkar and Princy Sasapara for the Interactive Data Science(https://dig.cmu.edu/ids2022) course at Carnegie Mellon University (https://www.cmu.edu).")
